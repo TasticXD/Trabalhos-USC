96ad62b6e68f7d018eaf6366a74683692212f704
TasticXD <162758302+TasticXD@users.noreply.github.com>
2024-03-15 21:45:10 -0300

    Create Atividade03.java

---
+++ Atividade03.java
@@ -0,0 +1,16 @@
+package aula2;
+
+public class AtividadeTreino {
+	public static void main(String[] args) {
+		String nome = "Victor";
+		int cpf = 1488599124;
+		int cep = 88145951;
+		String cidade = "Curitiba";
+		String estado = "PR";
+		
+		System.out.println("A pessoa com o nome " + nome + " registrada no cpf " + cpf + ", é residente do Cep " + cep + " na cidade de " + cidade + "-" + estado );
+		
+		
+	}
+
+}
