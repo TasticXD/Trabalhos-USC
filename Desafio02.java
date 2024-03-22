08901573563dcf5d5ad4ceaca54ca7555cfb2c69
TasticXD <162758302+TasticXD@users.noreply.github.com>
2024-03-22 19:51:48 -0300

    Desafio02.java

---
+++ Desafio02.java
@@ -0,0 +1,42 @@
+package Aula05;
+import java.util.Scanner;
+
+public class Desafio02 {
+   public static void main (String[] args) {
+  
+	   Scanner scanner = new Scanner(System.in);
+ 
+	   System.out.println("Digite sua idade: ");
+	   int idade = scanner.nextInt();
+	   
+       System.out.println("Digite seu batimento cardíaco: ");
+	   int batCard = scanner.nextInt();
+	   
+	   
+	   if (idade > 60 && batCard > 150) {
+		   System.out.println("URGÊNTE");
+	   
+	   }else if(idade > 60 && batCard < 150 ) {
+		   
+		   System.out.println("Urgência moderada");
+	   
+	   }else if(idade <= 60 && batCard < 150 ) {
+		   
+		   System.out.println("Urgência moderada");
+		   
+	   }else if (idade <= 60 && batCard <= 150 ) {
+		   
+		   System.out.println("Sem urgência ");
+		   
+	   }else {
+		   
+		   System.out.println("Ta tranquilo");
+	   }
+	   
+	   
+	   scanner.close();
+	   
+	   }
+	   
+   }
+   
