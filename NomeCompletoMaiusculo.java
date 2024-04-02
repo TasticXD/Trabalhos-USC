dbb9611713861ec7062094208033bbd6b28b8d56
TasticXD <162758302+TasticXD@users.noreply.github.com>
2024-04-02 13:52:18 -0300

    NomeCompletoMaiusculo.java

---
+++ NomeCompletoMaiusculo.java
@@ -0,0 +1,15 @@
+public static void main(String[] arg){
+    String nomeCompleto = "Seu nome";
+    String primeiroNome = "";
+    int i = 0;
+    boolean fim = false;
+    while ((i < nomeCompleto.length()) && !fim) {
+        if (nomeCompleto.charAt(i) != ' ') {
+            primeiroNome += nomeCompleto.charAt(i);
+            i++;
+        } else {
+            fim = true;
+        }
+    }
+    System.out.println(primeiroNome.toUpperCase());
+}
